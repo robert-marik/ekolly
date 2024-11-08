afc5d073af87132fbbb5587c14288acb15e07cee
Robert Marik <marik@mendelu.cz>
2024-11-08 15:50:54 +0100

    liska ostrovni

--- modules/nav.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def Navbar():
-    with st.sidebar:
-        st.page_link('app.py', label='Menu', icon='🚀')
-        st.page_link('pages/liska_ostrovni.py', label='Model lišky ostrovní', icon="🦊")
-        st.page_link('pages/Model_ostrovní_biogeografie.py', label='Model ostrovní biogeografie', icon="🐜")
-        st.page_link('pages/Logistický_růst_s_lovem.py', label='Logistický růst s lovem', icon="🕵️")
-        st.page_link('pages/Model_dravce_a_kořisti.py', label='Model dravce a kořisti', icon="🦈")
